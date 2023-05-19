76552f7a6fec333be7982ef51524f0306a6e7b3d
PlatonKynin <kininplaton@gmail.com>
2023-05-19 21:58:20 +0300

    factorial recursion

--- 03.Уроки_JavaScript/Рекурсия_и_стек_в_JavaScript_на_примерах_factorial,fibonacci,flatten/main.js
+++ 03.Уроки_JavaScript/Рекурсия_и_стек_в_JavaScript_на_примерах_factorial,fibonacci,flatten/main.js
@@ -1,6 +1,6 @@
 // Рекурсия и стек в JavaScript на примерах: factorial, fibonacci, flatten
 
-//1 factorial
+//1. factorial
 
 // 0! = 1
 // n! = n * (n - 1)!
@@ -9,10 +9,18 @@
 // 4! = 1 * 2 * 3 * 4 = 24;
 
 function factorial(n) {
+   if(n < 0) {
+      console.error('factorial вызван с неверным значение n'); //если число n < 0, то выдаст ошибку + бесконечный цикл
+   }
    if(n === 0) {
       return 1;
    } else {
       return n * factorial(n - 1); 
    }
 }
-factorial(3)
+console.log(factorial(4));
+
+
+//2. fibonacci
+
+// fibo
